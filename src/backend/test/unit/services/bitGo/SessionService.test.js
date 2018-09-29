05b679b5d2fdc78763bd685bdc688cf97731b602
Lucas Cunha <luccmir@gmail.com>
2018-09-28 23:41:49 -0300

    Add test to check configs

--- src/backend/test/unit/services/bitGo/SessionService.test.js
+++ src/backend/test/unit/services/bitGo/SessionService.test.js
@@ -5,6 +5,8 @@ describe('SessionService', () => {
   describe('#createSession', () => {
     it('returns an open session', () => {
       //TODO
+      test = config.get('test');
+      console.log(test);
     });
   });
 });
