325604e12d781bbd1fd5bf0492ea106ccc3cea35
Lucas Cunha <luccmir@gmail.com>
2018-09-29 20:17:18 -0300

    Add docs to API (swagger)

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var transfer = require('./routes/transfer');
 var faucet = require('./routes/faucet');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
@@ -20,6 +22,7 @@ app.use(cookieParser());
   routes
 */
 app.use('/transfer', transfer);
-app.use('/', faucet);
+app.use('/info', faucet);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
--- src/backend/test/integration/routes/faucet.test.js
+++ src/backend/test/integration/routes/faucet.test.js
@@ -8,14 +8,14 @@ describe('FaucetRoute', () => {
     jest.setTimeout(20000); // 20s
   });
 
-  describe('GET /', () => {
+  describe('GET /faucet', () => {
     it('returns status 200 (OK)', (done) => {
-      request(app).get('/')
+      request(app).get('/info')
         .expect(200, done);
     });
 
     it('returns response with faucet wallet info', (done) => {
-      request(app).get('/').end((err, res) => {
+      request(app).get('/info').end((err, res) => {
         var walletInfo = res.body;
         expect(walletInfo).toBeDefined();
         expect(walletInfo.id).toBe(faucetWalletId);
